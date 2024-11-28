915c098fbfa262470dc2037e73ae198d343606b3
Lisa-Dean-ABB2175_ferg <lisa.dean@supply,com>
2024-11-28 10:41:12 -0700

    more things

--- stow-shell/.config/shell/paths.sh
+++ stow-shell/.config/shell/paths.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 
-[[ -s ~/bin/$(uname -m) ]] && export PATH=~/bin/$(uname -m):$PATH
-[[ -s ~/bin/$(hostname) ]] && export PATH=~/bin/$(hostname):$PATH
-export PATH=~/bin:$PATH
+[[ -s $HOME/bin/$(uname -m) ]] && export PATH=$HOME/bin/$(uname -m):$PATH
+[[ -s $HOME/bin/$(hostname) ]] && export PATH=$HOME/bin/$(hostname):$PATH
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH" # VSCode command line
 export PATH="/usr/local/opt/openjdk/bin:$PATH" # OpenJDK
 export PATH=$HOME/.docker/bin:$PATH # Docker CLI
