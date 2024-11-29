520f684c4be0dd8c162b821ac900aa351c0d25d3
Lisa-Dean-ABB2175_ferg <lisa.dean@supply,com>
2024-11-29 15:46:19 -0700

    Add placeholder for Git configuration directory in setup script

--- setup/stow-all.sh
+++ setup/stow-all.sh
@@ -3,6 +3,8 @@
 cd "$DOTFILES"
 mkdir -p "$HOME/.config/shell/zsh"
 touch "$HOME/.config/shell/zsh/.placeholder"
+mkdir -p "$HOME/.config/git"
+touch "$HOME/.config/git/.placeholder"
 stow_dirs=("stow-home" "stow-shell" "stow-ssh" "stow-git" "stow-1password" "stow-bin")
 for dir in "${stow_dirs[@]}"; do
   stow -R "$dir"
