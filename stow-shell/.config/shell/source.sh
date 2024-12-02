6be354e3cbfc46be22c58c2692292776465254cf
Lisa Dean <lisa@lisadean.net>
2024-12-01 18:24:50 -0700

    add debug option

--- stow-shell/.config/shell/source.sh
+++ stow-shell/.config/shell/source.sh
@@ -2,5 +2,6 @@
 
 shellfiles=("secrets.sh" "vars.sh" "$(hostname).vars.sh" "paths.sh" "$(hostname).paths.sh" "aliases.sh" "$(hostname).aliases.sh")
 for file in "${shellfiles[@]}"; do
+  [[ -n $SHELL_DEBUG ]] && echo "DEBUG: Sourcing $file"
   [ -s "$SHELLDOTDIR/$file" ] && source "$SHELLDOTDIR/$file"
 done
