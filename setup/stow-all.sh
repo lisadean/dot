033884f4c6227271f78f4f15a8218aa424fe1ec7
Lisa Dean <lisa@lisadean.net>
2024-12-02 11:08:57 -0700

    update stow all script

--- setup/stow-all.sh
+++ setup/stow-all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
+export DOTFILES="${DOTFILES:-$HOME/dot}"
 export STOW_DIR="$DOTFILES/stow"
-export STOW_OPTIONS='--no-folding --verbose'
+export STOW_OPTIONS="--no-folding"
 export STOW_ACTION='-R' # -S --stow, -R --restow, -D --delete
 
-stow "$STOW_OPTIONS" -t "$HOME" $STOW_ACTION shell
-stow "$STOW_OPTIONS" -t "$HOME" $STOW_ACTION git
-stow "$STOW_OPTIONS" -t "$HOME" $STOW_ACTION 1password
-stow "$STOW_OPTIONS" -t "$HOME" $STOW_ACTION ssh
-stow "$STOW_OPTIONS" -t "$HOME" $STOW_ACTION docker
-stow "$STOW_OPTIONS" -t "/opt/datadog-agent/etc" $STOW_ACTION datadog
+stow $STOW_OPTIONS -d "$STOW_DIR" -t "$HOME" $STOW_ACTION shell
+stow $STOW_OPTIONS -d "$STOW_DIR" -t "$HOME" $STOW_ACTION git
+stow $STOW_OPTIONS -d "$STOW_DIR" -t "$HOME" $STOW_ACTION 1password
+stow $STOW_OPTIONS -d "$STOW_DIR" -t "$HOME" $STOW_ACTION ssh
+stow $STOW_OPTIONS -d "$STOW_DIR" -t "$HOME" $STOW_ACTION docker
+sudo stow $STOW_OPTIONS -d "$STOW_DIR" -t /opt $STOW_ACTION datadog
