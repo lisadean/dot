68040d10ce19ce6f534727d9c006221bb5145901
Lisa Dean <lisa@lisadean.net>
2026-02-23 10:24:29 -0500

    path for claude code

--- stow/shell/.config/shell/paths.sh
+++ stow/shell/.config/shell/paths.sh
@@ -3,6 +3,7 @@
 [[ -s $HOME/bin/$(uname -m) ]] && export PATH=$HOME/bin/$(uname -m):$PATH
 [[ -s $HOME/bin/$(hostname) ]] && export PATH=$HOME/bin/$(hostname):$PATH
 export PATH=$DOTFILES/bin:/usr/local/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH" # VSCode command line
 export PATH="/usr/local/opt/openjdk/bin:$PATH" # OpenJDK
 export PATH=$HOME/.docker/bin:$PATH # Docker CLI
